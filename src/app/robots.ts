efd5e3c6e26b8180994ea4f6628ef0ecb08ca6b9
Manu <manu@novatechafrica.com>
2026-03-24 10:29:10 +0300

    chore: final technical SEO updates for axelo.io domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://novatechafrica.com/sitemap.xml',
+    sitemap: 'https://axelo.io/sitemap.xml',
   }
 }
